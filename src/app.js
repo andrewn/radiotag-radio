be1833e40d7d016dac74055a475f552cc067ac5d
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-09-15 14:45:34 +0100

    Simple web UI

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 var states = require('./states'),
-    web    = require('./web');
+    Web    = require('./web');
+
+var web;
 
 module.exports = {
   init: function (config) {
     config = config || {};
     states.init();
 
-    web.init();
+    web = Web.create();
+    web.on('button', function (num) {
+      console.log('Button %s pressed', num);
+      if (num === '1') {
+        states.power();
+      }
+    });
   }
 };
--- src/states.js
+++ src/states.js
@@ -2,16 +2,18 @@ var StateMachine = require('javascript-state-machine');
 
 var fsm = StateMachine.create({
   initial:  { state: 'loading', event: 'init', defer: true },
+  error: function(eventName, from, to, args, errorCode, errorMessage) {
+    return 'event ' + eventName + ' not valid. ' + errorMessage;
+  },
   events: [
     { name: 'loaded', from: 'loading', to: 'standby' },
-    { name: 'power',  from: 'standby', to: 'starting' },
-    { name: 'started',  from: 'starting', to: 'playing' }
+    { name: 'power',  from: 'standby', to: 'playing' },
+    { name: 'power',  from: 'playing', to: 'standby' }
   ],
   callbacks: {
     oninit: init,
     onloaded: loaded,
-    onpower: power,
-    onstarted: started
+    onpower: power
   }
 });
 
@@ -28,10 +30,11 @@ function loaded(event, from, to, msg) {
 
 function power(event, from, to, msg) {
   log(arguments);
-}
-
-function started(event, from, to, msg) {
-  log(arguments);
+  if (to === 'playing') {
+    // start radio
+  } else if (to === 'standby') {
+    // stop radio
+  }
 }
 
 /* Helpers */
